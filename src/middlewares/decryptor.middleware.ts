8265b76a555657a6f1da86d33b9de750ee19d886
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-01-22 19:16:43 +0700

    fix:[major] update encryptor middleware

--- src/middlewares/decryptor.middleware.ts
+++ src/middlewares/decryptor.middleware.ts
@@ -1,13 +1,15 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NextFunction, Request } from "express";
-import { Encryptor } from "src/services/encryptor.service";
+import { EncryptorUtil } from "src/core/utils/modules/encryptor/encryptor.service";
+import { EnvService } from "src/infra/configs/env.service";
 
 @Injectable()
 export class DecryptorMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const isEnc = Number(req.headers.enc || "0");
     const ignoreFields = JSON.parse(String(req.headers.ignore || "[]"));
-    const encryptor = new Encryptor();
+    const encryptor = new EncryptorUtil(new EnvService(new ConfigService()));
     if (isEnc) {
       req.body = encryptor.doDecrypt(req.body, ignoreFields);
       next();
