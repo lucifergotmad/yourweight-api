121b8ac7f9ba124455752a7fc372dcd091f04c00
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-01-26 11:37:31 +0700

    chore:[minor] adding documentation to the dto response

--- src/modules/weight/controller/dtos/bmi.response.dto.ts
+++ src/modules/weight/controller/dtos/bmi.response.dto.ts
@@ -3,6 +3,15 @@ import { BMI } from "src/core/constants/app/bmi.const";
 import { IBMIResponse } from "src/interface-adapter/interfaces/weight/weight.interface";
 
 export class BMIResponseDTO implements IBMIResponse {
+  /**
+   *
+   * @param props {IBMIResponse}
+   *
+   * Transform Plain object into DTO useful for whitelisting data,
+   * this will avoid data leak, and preventing return a whole bunch
+   * of data to client.
+   */
+
   constructor(props: IBMIResponse) {
     this.description = props.description;
     this.index = props.index;
